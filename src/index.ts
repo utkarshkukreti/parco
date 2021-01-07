4781e9b004bbf162af3719df15d01deccb1d9aa9
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-01-07 13:30:25 +0530

    add class Parser

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export type Result<A> = Ok<A> | Error
 export type Ok<A> = { ok: true; input: string; value: A }
 
 export type Error = { ok: false; input: string; value: string }
+
+export class Parser<A> {
+  constructor(readonly fun: (input: string) => Result<A>) {}
+}
