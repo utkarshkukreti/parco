805060369cb6d8b1b5f3f26d28bd47da8a21e5d6
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-01-08 11:16:20 +0530

    add benchmark for json example using benchmark.js; ~2235 ops/s
        $ yarn tsc -P tsconfig.cjs.json && node dist/benchmarks/json.js 5
        [ { name: '31517 bytes', 'ops/s': 2213, 'mb/s': 66.53 } ]
        [ { name: '31517 bytes', 'ops/s': 2241, 'mb/s': 67.36 } ]
        [ { name: '31517 bytes', 'ops/s': 2242, 'mb/s': 67.4 } ]
        [ { name: '31517 bytes', 'ops/s': 2234, 'mb/s': 67.18 } ]
        [ { name: '31517 bytes', 'ops/s': 2227, 'mb/s': 66.95 } ]

---
+++ benchmarks/json.ts
@@ -0,0 +1,25 @@
+import * as Benchmark from 'benchmark'
+
+import Json from '../examples/json'
+import Sample from './json.sample'
+
+const times = +(process.argv[2] || '1')
+
+const inputs: string[] = [Sample]
+
+const run = (name: string, input: string, fn: () => unknown) => {
+  const benchmark = new Benchmark(fn).run({ async: false })
+  console.log([
+    {
+      name,
+      'ops/s': ~~benchmark.hz,
+      'mb/s': +((benchmark.hz * input.length) / 1048576).toFixed(2),
+    },
+  ])
+}
+
+for (const input of inputs) {
+  for (let i = 0; i < times; i++) {
+    run(`${input.length} bytes`, input, () => Json(input))
+  }
+}
