9fa2100252cc0c6538b2740c18caa531b1134275
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-01-08 11:21:56 +0530

    refactor json example

--- examples/json.ts
+++ examples/json.ts
@@ -6,7 +6,7 @@ const Keyword = P(/true|false|null/)
 const String = P(/"(:?[^\\"\t\n\r\x00]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/)
 const Number = P(/-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/)
 
-const ch = (string: string) => P(new RegExp(`[ \t\r\n]*${string}[ \t\r\n]*`))
+const ch = (string: string) => p.regex(`[ \t\r\n]*${string}[ \t\r\n]*`)
 
 const Array: p.Parser<Value[]> = ch('\\[')
   .skipThen(p.lazy(() => Value).array({ join: ch(',') }))
