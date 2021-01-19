2eee98327329775c82d1fa20f7b571e6dccfcdcc
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-01-19 17:41:26 +0530

    refactor arithmetic example

--- examples/arithmetic.ts
+++ examples/arithmetic.ts
@@ -5,19 +5,12 @@ const Integer = p.regex(/\d+/, { expected: 'an integer' }).map(Number)
 const Op = (string: string, fun: (l: number, r: number) => number) =>
   P(string).map(() => fun)
 
-const Expr3: p.Parser<number> = Integer.or(
+const Expr: p.Parser<number> = Integer.or(
   p.lazy(() => Expr).wrap(P('('), P(')')),
 )
-
-const Expr2 = Expr3.chainRight(Op('**', (l, r) => l ** r))
-
-const Expr1 = Expr2.chainLeft(
-  p.or([Op('*', (l, r) => l * r), Op('/', (l, r) => l / r)]),
-)
-
-const Expr = Expr1.chainLeft(
-  p.or([Op('+', (l, r) => l + r), Op('-', (l, r) => l - r)]),
-)
+  .chainRight(Op('**', (l, r) => l ** r))
+  .chainLeft(p.or([Op('*', (l, r) => l * r), Op('/', (l, r) => l / r)]))
+  .chainLeft(p.or([Op('+', (l, r) => l + r), Op('-', (l, r) => l - r)]))
 
 const Full = Expr.thenSkip(p.end())
 
