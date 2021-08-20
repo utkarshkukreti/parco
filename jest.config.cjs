57826a29f10fe32f181980223d9d084a01655b00
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-08-20 21:03:01 +0530

    run tests under esm

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 }
