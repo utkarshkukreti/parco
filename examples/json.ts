7ff6d378f10f4b2461bbc2352e7fbf61739584b0
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-08-20 22:09:48 +0530

    optimize json example using regex_: 113 => 126 mib/s on Node v16.7.0

--- examples/json.ts
+++ examples/json.ts
@@ -7,7 +7,7 @@ const String = P(/"(?:[^\\"\t\n\r\x00]+|\\(?:[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/)
 const Number = P(/-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/)
 
 const ch = (re: string, expected = re) =>
-  p.regex(`[ \t\r\n]*${re}[ \t\r\n]*`, { expected })
+  p.regex_(`[ \t\r\n]*${re}[ \t\r\n]*`, { expected })
 
 const Array = p
   .lazy(() => Value)
